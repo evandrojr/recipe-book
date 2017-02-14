062e4482ee79ac4ea396fbd2bb7e8d53c38383c7
Evandro Junior <evandrojr@gmail.com>
2017-02-14 06:39:40 -0300

    recipe list showing dummy

--- src/app/recipes/recipe-list/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Recipe } from './../recipe';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'rb-recipe-item',
-  templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+  templateUrl: './recipe-item.component.html'
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe;
+  recipeId: number;
 
   constructor() { }
 
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './../recipe';
 
 @Component({
   selector: 'rb-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
-  constructor() { }
+  recipes: Recipe[] = [];
+  recipe = new Recipe("Camarão no côco", "Camarões dentro do côco", "http://imagem.band.com.br/96/f_246696.jpg");
+  constructor() {
+    this.recipes.push(this.recipe);
+   }
 
   ngOnInit() {
   }
